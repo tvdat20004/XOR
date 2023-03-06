88e4fa52402561881a3f6bf801ba07b7122c091c
Thái Vĩnh Đạt <117071011+tvdat20004@users.noreply.github.com>
2023-03-06 19:27:20 +0700

    new_xor

--- XOR_hex2.py
+++ XOR_hex2.py
@@ -4,18 +4,6 @@ hex1 = "73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d"
 hex1 = bytes.fromhex(hex1)
 for key in range(256):
     result = bytes([byte ^ key for byte in hex1])
+    # convert the result back to hexadecimal string
     ascii_str = result.decode(errors='ignore')
     print(f"key={key}, ascii_str={ascii_str}")
-# perform the XOR operation
-# for num2 in range(255):
-#     result = num1 ^ num2
-#     hex_result = hex(result)[2:]
-#     if len(hex_result) < len(hex1):
-#         hex_result = '0' * (len(hex1) - len(hex_result)) + hex_result
-#     print(hex_result)
-
-# convert the result back to hexadecimal string
-
-# pad the result with leading zeroes if necessary
-
-
